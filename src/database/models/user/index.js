db11c94150468a3964083c8e47c41ff9a30cd786
Sofiaspinelli <sofiaspinelli17@gmail.com>
2023-08-17 16:50:45 -0300

    models de usuarios y productos

--- src/database/models/user/index.js
+++ src/database/models/user/index.js
@@ -1,1 +1,44 @@
-var user = ""
+import { Sequelize, DataTypes } from 'sequelize';
+
+const sequelize = new Sequelize('sqlite::memory:');
+const User = sequelize.define('User', {
+    id: DataTypes.INTEGER,
+  nombre: DataTypes.STRING,
+  email: DataTypes.DATE,
+  password:DataTypes.STRING,
+
+});
+
+module.exports = (sequelize, dataTypes) => {
+    const alias = "User";
+
+    const cols = {
+        id: {
+            type: dataTypes.INTEGER(11),
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        nombre: {
+            type: dataTypes.STRING(45),
+            allowNull: false,
+        },
+        email: {
+            type: dataTypes.STRING(60),
+            allowNull: false,
+        },
+        pass: {
+            type: dataTypes.STRING(70),
+            allowNull: false,
+        }
+    }
+
+    const config = {
+        tableName: "usuarios",
+        createdAt: "created_at",
+        updatedAt: "updated_at",
+    }
+
+    return sequelize.define(alias,cols,{...config})
+
+}
