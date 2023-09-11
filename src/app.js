a3e18585186a21d670bef9f46496675ea8d02d26
Yami <yamilaveralopez@gmail.com>
2023-09-11 19:32:39 -0300

    Fix validations updated photos

--- src/app.js
+++ src/app.js
@@ -40,6 +40,8 @@ app.use((err, req, res, next) => {
     res.status(500).json({ error: "Internal Server Error" });
 });
 
+
+
 // Iniciar servidor
 app.listen(PORT, () => console.log(`Server listen in port: ${PORT}\n http://localhost:${PORT}`));
 
--- src/routes/enterprise.js
+++ src/routes/enterprise.js
@@ -10,7 +10,6 @@ deleteEnterprise
 } = require("../controllers/enterprise");
 
 const { enterpriseValidationRules } = require("../validations/enterprise.validator");
-const validate = require("../validations/index.validator");
 // middleware para la carga de imagenes mediante multer
 let uploadEnterpriseFile = require('../middlewares/uploadEnterprisesFiles')
 
@@ -18,8 +17,8 @@ let uploadEnterpriseFile = require('../middlewares/uploadEnterprisesFiles')
 router
   .get("/", getEnterprises)
   .get("/:id", getEnterpriseById) 
-  .post("/create", uploadEnterpriseFile.fields([{ name: 'foto_card', maxCount: 1 }, { name: 'foto_emprendimiento', maxCount: 1 }]), enterpriseValidationRules(), validate, createEnterprise)
-  .put("/update/:id", uploadEnterpriseFile.fields([{ name: 'foto_card', maxCount: 1 }, { name: 'foto_emprendimiento', maxCount: 1 }]), enterpriseValidationRules(), validate, updateEnterprise)
+  .post("/create", uploadEnterpriseFile.fields([{ name: 'foto_card', maxCount: 1 }, { name: 'foto_emprendimiento', maxCount: 1 }]), enterpriseValidationRules(), createEnterprise)
+  .put("/update/:id", uploadEnterpriseFile.fields([{ name: 'foto_card', maxCount: 1 }, { name: 'foto_emprendimiento', maxCount: 1 }]), enterpriseValidationRules(), updateEnterprise)
   .delete("/delete/:id", deleteEnterprise);
 
 module.exports = router;
