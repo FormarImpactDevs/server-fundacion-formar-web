db96f5a1ef87e8f4ee57fee36bef056f49c81445
JonatanCespedes <retratos.jonatan@gmail.com>
2025-04-02 15:58:59 -0300

    refactor: simplify order validation by removing unnecessary checks

--- src/validations/order.validator.js
+++ src/validations/order.validator.js
@@ -3,15 +3,6 @@ const { body } = require('express-validator');
 const validateCreateOrder = [
   body('tipo_de_entrega').isIn(['point', 'delivery']).withMessage('El tipo de entrega debe ser "point" o "delivery"'),
   body('client_data').notEmpty().withMessage('Los datos del cliente son obligatorios'),
-  
-  body('punto_retiro_id').custom((value)=> {
-    if (value === null || value === undefined) {
-      return true; // Si el valor es null o undefined, se permite
-    }
-    if (typeof value !== 'number') {
-      throw new Error('El ID del punto de retiro debe ser un número');
-    }
-  }).withMessage('El ID del punto de retiro debe ser un número'),
 ];
 
 const validateUpdateOrder = [
